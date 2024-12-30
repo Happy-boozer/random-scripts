674fce12db4bee61741a3f88f263bc6f2ce5f566
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2024-12-30 22:17:51 +0300

    Add files via upload

---
+++ renaming_v2.py
@@ -0,0 +1,31 @@
+import os # библиотека для работы с системой
+with open("prefixsis.txt", mode="r") as f:
+    s = f.readlines()
+
+prefixsis = []
+kolvo = 0
+for i in s:
+    prefixsis.append(i.rstrip())
+
+way = f"H:\CD07"#путь до папки
+for filename in os.listdir(way): #читаем имена файлов
+    f = os.path.join(way, filename)# соединяем имя файла и путь к нему
+    r = f[-3:]# записываем расширение файла
+    kolvo += 1
+
+    if kolvo < 100 and filename[0] != "0":
+        new_name = "H:\CD07\\"
+        new_name += "0"
+        new_name += str(kolvo)
+        new_name += "."
+        new_name += r
+        os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)# переименовываем файл
+        print(filename)
+        print(new_name)
+    elif kolvo >= 100 and filename[0] != "0":
+        new_name = f"H:\CD07\{kolvo}.{r}"# новое имя файла вместе с путём к нему
+        os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)# переименовываем файл
+        print(filename)
+        print(new_name)
+    else:
+        break
