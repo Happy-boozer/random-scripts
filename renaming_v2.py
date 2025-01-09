3851b32ba3aea8b3e02862b49713bd4ebf76b4f0
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-01-09 15:14:01 +0300

    Update renaming_v2.py

--- renaming_v2.py
+++ renaming_v2.py
@@ -2,23 +2,22 @@ import os # библиотека для работы с системой
 
 kolvo = 0
 
-way = f"H:\CD07"#путь до папки
+way = f"G:\CD02"#путь до папки
 for filename in os.listdir(way): #читаем имена файлов
     f = os.path.join(way, filename)# соединяем имя файла и путь к нему
     r = f[-3:]# записываем расширение файла
     kolvo += 1
 
     if kolvo < 100 and filename[0] != "0":
-        new_name = "H:\CD07\\"
-        new_name += "0"
-        new_name += str(kolvo)
-        new_name += "."
-        new_name += r
-        os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)# переименовываем файл
-        print(filename)
-        print(new_name)
+        if kolvo < 10:
+            nn = "00"
+            new_name = f"{way}\{nn}{kolvo}.{r}"
+            os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)  # переименовываем файл
+        else:
+            new_name = way
+            n = "0"
+            new_name = f"{way}\{n}{kolvo}.{r}"
+            os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)  # переименовываем файл
     elif kolvo >= 100 and filename[0] != "0":
-        new_name = f"H:\CD07\{kolvo}.{r}"# новое имя файла вместе с путём к нему
+        new_name = f"G:\CD02\{kolvo}.{r}"# новое имя файла вместе с путём к нему
         os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)# переименовываем файл
-        print(filename)
-        print(new_name)
