4ab09c9222eda307e12544a3afb7abc53cdf67aa
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-01-21 11:50:47 +0300

    Update cheking.py

--- cheking.py
+++ cheking.py
@@ -1,19 +1,20 @@
 import os # библиотека для работы с системой
 
 way = r"H:" #путь к папкам
-for dirname in os.listdir(way): #читаем имена папок
-    if dirname[0:2] == "CD":
-        files = []
-        new_way = way + "\\" #создаём путь в которой будем считать файлы
-        new_way += dirname #приклеиваем имя папки
-        for filename in os.listdir(new_way):
+def checking(way):
+    for dirname in os.listdir(way): #читаем имена папок
+        if dirname[0:2] == "CD":
+            files = []
+            new_way = way + "\\" #создаём путь в которой будем считать файлы
+            new_way += dirname #приклеиваем имя папки
+            for filename in os.listdir(new_way):
             # считываем имена файлов внутри папки
-            files.append(filename[0:3])
+                files.append(filename[0:3])
 
-        files = sorted(files)
-        for i in range(len(files)-1):
-            if int(files[i]) < int(files[i+1]):
-                continue
-            else:
-                print("Error")
-                print(new_way, files[i], files[i+1])
+            files = sorted(files)
+            for i in range(len(files)-1):
+                if int(files[i]) < int(files[i+1]):
+                    continue
+                else:
+                    print("Error")
+                    print(new_way, files[i], files[i+1])
