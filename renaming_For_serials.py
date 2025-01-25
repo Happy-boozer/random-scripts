8c9525463e33bdaa4360c2d5c4b8e43f6198af06
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-01-25 11:50:42 +0300

    Update renaming_For_serials.py

--- renaming_For_serials.py
+++ renaming_For_serials.py
@@ -3,25 +3,26 @@ import os # библиотека для работы с системой
 kolvo = 0
 
 way = f"G:\фильмы\Slovo.pacana.Krov.na.asfalte.S01.2023.WEB-DL.1080p"#путь до папки
-for filename in os.listdir(way): #читаем имена файлов
-    f = os.path.join(way, filename)# соединяем имя файла и путь к нему
-    r = f[-3:]# записываем расширение файла
-    kolvo += 1
+def renaming_series(way):
+    for filename in os.listdir(way): #читаем имена файлов
+        f = os.path.join(way, filename)# соединяем имя файла и путь к нему
+        r = f[-3:]# записываем расширение файла
+        kolvo += 1
 
-    if kolvo < 100 and filename[0] != "0":
-        if kolvo < 10:
-            nn = "00"
+        if kolvo < 100 and filename[0] != "0":
+            if kolvo < 10:
+                nn = "00"
+                seri = "_серия"
+                new_name = f"{way}\{nn}{kolvo}{seri}.{r}"
+                os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)  # переименовываем файл
+            else:
+                new_name = way
+                n = "0"
+                seri = "_серия"
+                new_name = f"{way}\{n}{kolvo}{seri}.{r}"
+                os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)  # переименовываем файл
+        elif kolvo >= 100 and filename[0] != "0":
             seri = "_серия"
-            new_name = f"{way}\{nn}{kolvo}{seri}.{r}"
-            os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)  # переименовываем файл
-        else:
-            new_name = way
-            n = "0"
-            seri = "_серия"
-            new_name = f"{way}\{n}{kolvo}{seri}.{r}"
-            os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)  # переименовываем файл
-    elif kolvo >= 100 and filename[0] != "0":
-        seri = "_серия"
-        new_name = f"{way}\{kolvo}{seri}.{r}"# новое имя файла вместе с путём к нему
-        os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)# переименовываем файл
-    print(filename)
+            new_name = f"{way}\{kolvo}{seri}.{r}"# новое имя файла вместе с путём к нему
+            os.rename(f, new_name, src_dir_fd=None, dst_dir_fd=None)# переименовываем файл
+    #print(filename)
