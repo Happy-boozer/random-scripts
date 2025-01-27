a0c3460c28b3e02998c36ea8f1129ad30c21cbb3
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-01-27 17:16:18 +0300

    Add files via upload

---
+++ equalizer.py
@@ -0,0 +1,24 @@
+import os
+import counting
+import shutil
+
+def equalization(bol,men,mk):
+    for filename in os.listdir(bol):
+        if int(filename[0:3]) > mk:
+            shutil.copy2(f"{bol}/{filename}", f"{men}/{filename}")#копируем файл из одной папки в другую
+
+
+way1 = "L:"
+way2 = "G:"
+
+for i in range(1, 9):
+    new_way_1 = f"{way1}/CD0{i}"
+    new_way_2 = f"{way2}/CD0{i}"
+    kolv01 = counting.counting_one_dir(new_way_1)#подсчитываем количество файлов в папке
+    kolv02 = counting.counting_one_dir(new_way_2)#подсчитываем количество файлов в папке
+    if kolv01 == kolv02:
+        continue
+    elif kolv01 > kolv02:
+        equalization(new_way_1, new_way_2, kolv02)
+    elif kolv01 < kolv02:
+        equalization(new_way_2, new_way_1, kolv01)
