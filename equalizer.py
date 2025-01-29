ee77bf3f55c5918e0f94ffe6cee20b0c2993c191
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-01-29 13:52:15 +0300

    Update equalizer.py

--- equalizer.py
+++ equalizer.py
@@ -11,14 +11,15 @@ def equalization(bol,men,mk):
 way1 = "L:"
 way2 = "G:"
 
-for i in range(1, 9):
-    new_way_1 = f"{way1}/CD0{i}"
-    new_way_2 = f"{way2}/CD0{i}"
-    kolv01 = counting.counting_one_dir(new_way_1)#подсчитываем количество файлов в папке
-    kolv02 = counting.counting_one_dir(new_way_2)#подсчитываем количество файлов в папке
-    if kolv01 == kolv02:
-        continue
-    elif kolv01 > kolv02:
-        equalization(new_way_1, new_way_2, kolv02)
-    elif kolv01 < kolv02:
-        equalization(new_way_2, new_way_1, kolv01)
+def equalizer(way1, way2):
+    for i in range(1, 9):
+        new_way_1 = f"{way1}/CD0{i}"
+        new_way_2 = f"{way2}/CD0{i}"
+        kolv01 = counting.counting_one_dir(new_way_1)#подсчитываем количество файлов в папке
+        kolv02 = counting.counting_one_dir(new_way_2)#подсчитываем количество файлов в папке
+        if kolv01 == kolv02:
+            continue
+        elif kolv01 > kolv02:
+            equalization(new_way_1, new_way_2, kolv02)
+        elif kolv01 < kolv02:
+            equalization(new_way_2, new_way_1, kolv01)
