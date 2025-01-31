4fd68991538cb0d421de3b36e1f18d3f8c2c12c3
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-01-31 23:41:11 +0300

    Update counting.py

--- counting.py
+++ counting.py
@@ -6,10 +6,8 @@ def counting(way):
     for dirname in os.listdir(way): #читаем имена папок
         kolvo = 0
         if dirname[0:2] == "CD":
-        #print(dirname)
             new_way = "G:\\"#создаём путь в которой будем считать файлы
             new_way += dirname #приклеиваем имя папки
-        #print(new_way)
             for filename in os.listdir(new_way):# считываем имена файлов внутри папки
                 kolvo += 1#считаем количество фалов внутри папки
         itogo += kolvo
