ea64a6d67c68d0952664035bf5d6c3a0945e43e3
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-02-01 13:40:35 +0300

    Update year_sorter.java

--- year_sorter.java
+++ year_sorter.java
@@ -11,19 +11,15 @@ public class dire {
         for(File item : papka.listFiles()){
             if(photo_year.equals(item.getName())){
                 File TARGET_FILE = new File("C:\\Users\\Alexandr\\Desktop\\photo\\"+item.getName() + "\\"+filname);
-                //boolean isMoved = FILE_TO_MOVE.renameTo(new File(TARGET_FILE.getName()));
                 System.out.println("filname");
                 result = FILE_TO_MOVE.renameTo(TARGET_FILE);
-                //result = true;
 
             }
             else {
                 File podpapka = new File("C:\\Users\\Alexandr\\Desktop\\photo\\"+photo_year);
                 if (podpapka.mkdir()){
                     File TARGET_FILE = new File("C:\\Users\\Alexandr\\Desktop\\photo\\"+photo_year+"\\"+filname);
-                    //System.out.println(TARGET_FILE.getName());
                     result = FILE_TO_MOVE.renameTo(TARGET_FILE);
-                //result = true;
                 }
             }
         }
