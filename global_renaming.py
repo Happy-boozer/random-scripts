6a0078cbbe9dc58a69445c9461b2043500dd4bd1
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-02-09 21:52:15 +0300

    Update global_renaming.py

--- global_renaming.py
+++ global_renaming.py
@@ -5,7 +5,6 @@ def renaming(way):
 for dirname in os.listdir(way): #читаем имена папок
     kolvo = 0
     if dirname[0:2] == "CD":
-        #print(dirname)
         new_way = way + "\\" #создаём путь в которой будем считать файлы
         new_way += dirname #приклеиваем имя папки
         for filename in os.listdir(new_way):
