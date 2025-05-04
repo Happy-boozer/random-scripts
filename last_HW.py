96a40ea54720dbb992586a9bb8fc7c1dfb9f14a8
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2025-05-04 22:47:56 +0300

    Update last_HW.py

--- last_HW.py
+++ last_HW.py
@@ -4,14 +4,14 @@ class mur():
     p_z = 0
     p_y = 0
     kolvo_per = 0
+    output = "b0"
     lucky_draw = 0
     lucky_spawn = 0
     lucky_chip = 0
     lucky_stay = 0
     lucky_wreck = 0
 
-    def main(self):
-        return self
+    names_of_methods = ["draw", "chip", "stay", "spawn", "wreck"]
 
     def get_step(self):
         return self.kolvo_per
@@ -29,81 +29,93 @@ class mur():
         return None
 
     def draw(self):
-        if self.sost == "R5" and self.p_v == 0:
+        if self.sost == "R5" and self.p_v == 0 and self.output == "b0":
             self.sost = "R2"
             self.kolvo_per += 1
             self.lucky_draw += 1
+            self.output = "b2"
             return None
-        elif self.sost == "R5" and self.p_v == 1:
+        elif self.sost == "R5" and self.p_v == 1 and self.output == "b0":
             self.sost = "R7"
             self.kolvo_per += 1
             self.lucky_draw += 1
+            self.output = "b0"
             return None
-        elif self.sost == "R4":
+        elif self.sost == "R4" and self.output == "b3":
             self.sost = "R0"
             self.kolvo_per += 1
             self.lucky_draw += 1
+            self.output = "b4"
             return None
-        elif self.sost == "R7":
+        elif self.sost == "R7" and self.output == "b0":
             self.sost = "R3"
             self.kolvo_per += 1
             self.lucky_draw += 1
+            self.output = "b0"
             return None
-        elif self.sost == "R3":
+        elif self.sost == "R3" and self.output == "b0":
             self.sost = "R1"
             self.kolvo_per += 1
             self.lucky_draw += 1
+            self.output = "b3"
             return None
         else:
             return "unsupported"
 
     def stay(self):
-        if self.sost == "R1":
+        if self.sost == "R1" and self.output == "b3":
             self.sost = "R6"
             self.kolvo_per += 1
             self.lucky_stay += 1
+            self.output = "b6"
             return None
-        elif self.sost == "R6":
+        elif self.sost == "R6" and self.output == "b6":
             self.sost = "R5"
             self.kolvo_per += 1
             self.lucky_stay += 1
+            self.output = "b0"
             return None
         else:
             return "unsupported"
 
     def chip(self):
-        if self.sost == "R2" and self.p_z == 1:
+        if self.sost == "R2" and self.p_z == 1 and self.output == "b2":
             self.sost = "R4"
             self.kolvo_per += 1
             self.lucky_chip += 1
+            self.output = "b3"
             return None
-        elif self.sost == "R2" and self.p_z == 0:
+        elif self.sost == "R2" and self.p_z == 0 and self.output == "b2":
             self.sost = "R7"
             self.kolvo_per += 1
             self.lucky_chip += 1
+            self.output = "b0"
             return None
         else:
             return "unsupported"
 
     def spawn(self):
-        if self.sost == "R2" and self.p_y == 0:
+        if self.sost == "R0" and self.p_y == 0 and self.output == "b4":
             self.sost = "R7"
             self.kolvo_per += 1
             self.lucky_spawn += 1
+            self.output = "b0"
             return None
-        elif self.sost == "R7" and self.p_y == 1:
+        elif self.sost == "R0" and self.p_y == 1 and self.output == "b4":
             self.sost = "R3"
             self.kolvo_per += 1
             self.lucky_spawn += 1
+            self.output = "b0"
             return None
         else:
             return "unsupported"
 
     def wreck(self):
-        if self.sost == "R4":
+        if self.sost == "R4" and self.output == "b3":
             self.sost = "R6"
             self.kolvo_per += 1
             self.lucky_wreck += 1
+            self.output = "b3"
             return None
         else:
             return "unsupported"
@@ -120,3 +132,14 @@ class mur():
                 return self.lucky_spawn
             case "draw":
                 return self.lucky_draw
+
+    def __getattr__(self, item):
+        if item not in self.names_of_methods:
+            return lambda: "unknown"
+
+    def get_output(self):
+        return self.output
+
+
+def main():
+    return mur()
